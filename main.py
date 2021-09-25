045c676970d1a0a3ae50abb379841617efe84da4
Simon Christiansen <sich97@gmail.com>
2021-09-25 15:39:42 +0200

    Fixed issue with rounding function

--- main.py
+++ main.py
@@ -128,93 +128,10 @@ def simulate(amount_of_attacks, enemy_ac_lower, enemy_ac_higher, attack_bonus, d
     return stats, min_damage, max_damage
 
 
-def modify_string(original_string, index, value):
-    new_string = list(original_string)
-    for i in range(0, len(new_string)):
-        if i == index:
-            new_string[i] = value
-
-    resulting_string = "".join(new_string)
-    return resulting_string
-
-
-def precise_rounding(digit_whole, digit_decimals=0, precision=0):
-    # Turn the input into a float, and then into a string
-    digit_str = str(digit_whole) + "." + str(digit_decimals)
-
-    # Find the index location of the comma
-    comma_index = digit_str.index(".")
-    # Determine the index location of the value which will be rounded up/down
-    target_index = comma_index + precision
-
-    # Some quick analytical results
-    if target_index > len(digit_str):
-        return digit_whole, digit_decimals
-    elif target_index < 0:
-        return 0, 0
-
-    else:
-        # If we're 'moving backwards', move back one more to account for the comma
-        if precision < 1:
-            target_index -= 1
-
-        target_int = int(digit_str[target_index])
-
-        # Figure out the value of the number which is next(behind) of the target index
-        try:
-            trailing_int = int(digit_str[target_index + 1])
-        # If we get a value error, it's because of the comma, in which case we instead look one more index further
-        except ValueError:
-            trailing_int = int(digit_str[target_index + 2])
-
-        # If the trailing int is above 5, increment the target index by 1
-        if trailing_int > 5:
-            new_target_int = target_int + 1
-            digit_str = modify_string(digit_str, target_index, str(new_target_int))
-        # If the trailing int is exactly 5, increment the target index by 1 only if that makes an even number
-        elif trailing_int == 5:
-            if (target_int + 1) % 2 == 0:
-                new_target_int = target_int + 1
-                digit_str = modify_string(digit_str, target_index, str(new_target_int))
-
-        # This if statement accounts for the fence-post problem in the for loop beneath
-        if len(digit_str) - 1 == target_index + 1:
-            if not digit_str[-1] == ".":
-                digit_str = modify_string(digit_str, len(digit_str) - 1, "0")
-        else:
-            # Set all numbers behind the target index to 0
-            for index in range(target_index + 1, len(digit_str)):
-                if not digit_str[index] == ".":
-                    digit_str = modify_string(digit_str, index, "0")
-
-        # Find the start of the trailing zeroes
-        start_trailing_zeroes = comma_index + 1
-        for index in range(len(digit_str) - 1, 0, -1):
-            if digit_str[index] != "0":
-                start_trailing_zeroes = index + 1
-                break
-
-        # Remove trailing zeroes
-        digit_str = digit_str[:start_trailing_zeroes]
-        if digit_str[-1] == ".":
-            digit_str = digit_str[:comma_index]
-
-        # Return the resulting whole and decimal numbers
-        digit_str_split = digit_str.split(".")
-        resulting_digit_whole = int(digit_str_split[0])
-        if len(digit_str_split) < 2:
-            resulting_digit_decimals = 0
-        else:
-            resulting_digit_decimals = int(digit_str_split[1])
-        return resulting_digit_whole, resulting_digit_decimals
-
-
 def to_percentage(frequency, max_frequency, precision):
     decimal = frequency / max_frequency
     decimal *= 100
-    whole = int(str(decimal).split(".")[0])
-    decimal = int(str(decimal).split(".")[1])
-    percentage = precise_rounding(whole, decimal, precision)[0]
+    percentage = int(round(decimal))
     return percentage
 
 
